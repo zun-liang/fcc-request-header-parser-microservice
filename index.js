d66184706d9a51be815439a1ea337c2421630621
Zun Liang <107293493+zun-liang@users.noreply.github.com>
2024-09-03 22:05:56 -0500

    format code

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ app.get("/api/whoami", (req, res) => {
   const ipaddress = req.ip;
   const language = req.headers["accept-language"];
   const software = req.headers["user-agent"];
-
   res.status(StatusCodes.OK).json({ ipaddress, language, software });
 });
 
